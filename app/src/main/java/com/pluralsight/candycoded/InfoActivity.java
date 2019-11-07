ce64fa251c373a58132f1554aacf20a85579d378
amugabe <allanmugabe10@gmail.com>
2019-11-07 18:12:11 +0300

    Module_completed

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -37,4 +37,14 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+
+    // ***
+    // TODO - Task 3 - Launch the Phone Activity
+    // ***
+    public void createPhoneIntent(View view){
+        Intent phoneIntent=new Intent((Intent.ACTION_DIAL));
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+
+    }
 }
